b433f1572aa6f4b097d82a5d9f78236053bd9f1e
sulisindriyani <sulisindriyani04@gmail.com>
2024-09-11 17:11:12 +0700

    Update login-script.js
    mengganti komentar dengan fptmat jsDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -3,23 +3,67 @@
  * Gunakan fungsi goToHome() untuk menampilkan halaman home
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @modul login-script */
+/**
+* Membuat variabel loginFormElement untuk tampulam form.
+* @constant { HTMLElement
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/**
+* membuat variabel inputEmailElement untuk tampilan input email.
+*@constant { HTMLElement
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/**
+** membuat variabel inputPasswordElement untuk tampilan input password.
+*@constant { HTMLElement
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+/**
+** membuat variabel expectedEmail untuk menyimpan infprmasi email sementara.
+*@constant {string
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+
+/**
+** membuat variabel expectedPassword untuk menyimpan infprmasi password sementara.
+*@constant {string
+*/
 const expectedPassword = 'superpassword';
+
+
+
+/*comnet : menambahkan aksil klik pada button */
 
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  
+/**
+** membuat variabel email untuk menyimpan nilai email yang di dapatkan saat button di tekan.
+*@constant {string
+*/
+
   const email = inputEmailElement.value;
+
+  /**
+** membuat variabel email untuk menyimpan nilai password yang di dapatkan saat button di tekan.
+*@constant {string
+*/
+
   const password = inputPasswordElement.value;
+ /*comnet : memastikan bahwa nilai email dan password sesui dengan nilai yang tersimpan */ 
   if (email == expectedEmail && password == expectedPassword) {
+
+    /*comnet : jika sesuai maka program akan berpindah ke halaman home */
     goToHome();
   } else {
+    /*comnet : namun jika tidak sesuai makan akan menampilkan informasi bahwa input salah */
     showPopUp();
   }
 });
